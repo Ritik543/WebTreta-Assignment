afc42a89b6281674643c09f5e2048713c91f1680
RITIK SINGH <132573696+Ritik543@users.noreply.github.com>
2024-11-21 10:13:35 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+"use client";
+import { useState } from "react";
 import Navbar from "../app/components/Navbar";
-import Header from  "../app/components/Header";
+import Header from "../app/components/Header";
 import Features from "./components/Features";
 import Herosection from "./components/Herosection";
 import Application from "./components/Application";
@@ -8,26 +10,33 @@ import Differentnformation from "./components/Differentnformation";
 import Frequentyqustion from "./components/Frequentyqustion";
 import InfoBox from "./components/informationbox";
 import Footer from "./components/Footer";
-// import Frequentyqustion from "./components/Frequentyqustion";
 
 export default function Home() {
-  return (
-    <div className="bg-white min-h-screen overflow-hidden">
-      <Navbar />
-      <Header />
-      <Herosection/>
-      <Features />
-      <div className="p-10 mt-10">
-      <Application/> 
-      </div>
-      <Workingautoclick/>
-      <Differentnformation/>
-      <Frequentyqustion/> 
-      <InfoBox/>
-      <Footer/>
-      
-      
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  // Function to toggle dark mode
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <div
+      className={`${
+        isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+      } min-h-screen overflow-hidden`}
+    >
+      <Navbar isDarkMode={isDarkMode} onDarkModeToggle={toggleDarkMode} />
+      <Header isDarkMode={isDarkMode} />
+      <Herosection isDarkMode={isDarkMode} />
+      <Features isDarkMode={isDarkMode} />
+      <div className="p-10 mt-10">
+        <Application isDarkMode={isDarkMode} />
+      </div>
+      <Workingautoclick isDarkMode={isDarkMode} />
+      <Differentnformation isDarkMode={isDarkMode} />
+      <Frequentyqustion isDarkMode={isDarkMode} />
+      <InfoBox isDarkMode={isDarkMode} />
+      <Footer isDarkMode={isDarkMode} />
     </div>
   );
 }
